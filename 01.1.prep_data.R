0279eae3476ecd8ad2cda5de2ffda54c5c8362c9
Kerry Wong <41377220+wongkerry@users.noreply.github.com>
2024-01-29 18:19:59 +1100

    Update 01.1.prep_data.R

--- 01.1.prep_data.R
+++ 01.1.prep_data.R
@@ -19,7 +19,7 @@ windowsFonts(`Segoe UI` = windowsFont('Segoe UI'))
 
 setwd("C:\\Users\\wkerr\\Dropbox\\OnTIME local\\papers\\equity_paper")
 
-s2.google <- read.csv("C:\\Users\\wkerr\\Dropbox\\OnTIME local\\Data\\most recent\\s2cell_metrics_20230626.csv")
+#s2 cell data is available from figshare (see ref 43)
 s2 <- as.data.table(s2.google)
 
 #remove dup s2 cells  
